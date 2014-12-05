c092cc878f4923f4c24aeed1cfa49da10f9cec66
David Buckley <isreal-git@bucko.me.uk>
2014-12-05 17:50:39 +0000

    super secritter password with salty dawgs

--- public_html/id.pl
+++ public_html/id.pl
@@ -14,7 +14,7 @@ my $ident = $q->param('ident');
 
 print "Content-type: text/plain\n\n";
 
-if (md5_hex($ident) eq 'a66c606e8b7f34ac2294a78f34f42a98') {
+if (md5_hex('REPO_SALT_IS_FCKING_AWESOME'.$ident) eq 'c32d9dba435d9d3d6e66f5e511a32986') {
 	$dbi->do("UPDATE sessions SET admin=1 WHERE id=?", {}, $sess_id);
 	print "Done.\n";
 } else {
